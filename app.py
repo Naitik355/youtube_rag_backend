50f65de19954c249381fb3201d6f11f4866907ed
unknown <aggarwalnaitik993@gmail.com>
2026-07-22 11:58:25 +0530

    fix

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from rag import ask_video,load_video
 import traceback
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -47,5 +48,10 @@ def ask():
         }), 500
 
 
-if __name__ == "__main__":
-    app.run(debug=True,use_reloader=False)
+if __name__=="__main__":
+    port = int(os.environ.get("PORT", 5000))
+
+    app.run(
+        host="0.0.0.0",
+        port=port
+    )
